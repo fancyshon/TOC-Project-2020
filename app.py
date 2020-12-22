f34e26111fd2b0b6404423231c5b21724cd5b0a1
shon <10288show@gmail.com>
2020-12-22 22:50:27 +0800

    python3 changed

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def webhook_handler():
            graph.add_edge("A", "B")
            graph.add_edge("A", "G")
            graph.layout()
-           graph.draw("output.png",prog="neato")                   
+           graph.draw("output.png",prog = "neato")                   
         elif response == False:
             send_text_message(event.reply_token, "Not Entering any State")
 
