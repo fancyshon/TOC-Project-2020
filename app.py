1b2a1100c5d3f4fea49e04d19eae547dd914f0fa
shon <10288show@gmail.com>
2020-12-24 09:40:03 +0800

    fsm v1

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ load_dotenv()
 machine = TocMachine(
     states=["user", "intro", "begin",
     "1", "2", "3", "4", "5",
-    "part1","part2_1","part2_2","part3_1","part3_2","part4_1","part4_2",
+    "part1", "part2_1", "part2_2", "part3_1", "part3_2", "part4_1", "part4_2",
+    "french_kiss", "cheek_kiss", "do_nothin", "concern_about",
+    "answer", "embarassed", "bully",
+    "secret","open_bag","love_letter"
+    "good_ending","bad_ending","suicide_ending",
      "state1", "state2"],
     transitions=[
         {
@@ -50,26 +54,91 @@ machine = TocMachine(
         {
             "trigger": "go_to_part1", "source": "begin", "dest": "part1",
         },
+        #學藝真心話大冒險
         {
             "trigger": "go_to_part2_1", "source": "part1", "dest": "part2_1",
         },
         {
-            "trigger": "go_to_part2_2", "source": "part1", "dest": "part2_2",
+            "trigger": "truth", "source": "part2_1", "dest": "part3_1",
+        },
+        {
+            "trigger": "ans", "source": "truth", "dest": "answer",
+        },
+        {
+            "trigger": "os", "source": "answer", "dest": "embarassed",
+        },
+        {
+            "trigger": "laugh_at", "source": "answer", "dest": "bully",
+        },
+        {
+            "trigger": "nothing", "source": "bully", "dest": "suicide_ending",
+        },
+        {
+            "trigger": "concern", "source": "bully", "dest": "good_ending",
+        },
+        {
+            "trigger": "equal", "source": "embarassed", "dest": "cheek_kiss",
         },
 
         {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
+            "trigger": "dare", "source": "part2_1", "dest": "part3_2",
         },
         {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
+            "trigger": "french_kiss", "source": "part3_2", "dest": "french_kiss",
+        },
+        {
+            "trigger": "nothing", "source": "french_kiss", "dest": "bad_ending",
+        },
+        {
+            "trigger": "concern", "source": "french_kiss", "dest": "good_ending",
+        },
+        {
+            "trigger": "kiss", "source": "part3_2", "dest": "cheek_kiss",
+        },
+        {
+            "trigger": "kidding", "source": "cheek_kiss", "dest": "bad_ending",
+        },
+        {
+            "trigger": "concern", "source": "cheek_kiss", "dest": "good_ending",
+        },
+        #班長真心話大冒險
+        {
+            "trigger": "go_to_part2_2", "source": "part1", "dest": "part2_2",
         },
         
+        {
+            "trigger": "truth", "source": "part2_2", "dest": "part4_1",
+        },
+        {
+            "trigger": "tell_secret", "source": "part4_1", "dest": "secret",
+        },
+        {
+            "trigger": "stop", "source": "secret", "dest": "good_ending",
+        },
+        {
+            "trigger": "stop", "source": "secret", "dest": "good_ending",
+        },
+        {
+            "trigger": "murmur", "source": "secret", "dest": "cheek_kiss",
+        },
+        {
+            "trigger": "laugh", "source": "secret", "dest": "bully",
+        },
+        {
+            "trigger": "dare", "source": "part2_2", "dest": "part4_2",
+        },
+        {
+            "trigger": "open", "source": "part4_2", "dest": "open_bag",
+        },
+        {
+            "trigger": "drop", "source": "open_bag", "dest": "love_letter",
+        },
+        {
+            "trigger": "gossip", "source": "love_letter", "dest": "bully",
+        },
+        {
+            "trigger": "be_sympathy", "source": "love_letter", "dest": "cheek",
+        },
         
         {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
     ],
